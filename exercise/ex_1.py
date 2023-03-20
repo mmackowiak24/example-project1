85bac3153ff62e869025b764ca63ff35de731912
Mikołaj Maćkowiak <73481@student.ue.poznan.pl>
2023-03-20 12:00:45 +0100

    Added first task

---
+++ exercise/ex_1.py
@@ -0,0 +1,12 @@
+def hello(a: str) -> str:
+    # TODO
+    return "Hello"
+
+
+print("Example")
+print(hello("Ola"))
+
+assert hello("Ola") == "Hello Ola"
+assert hello("Zuzia") == "Hello Zuzia"
+
+print("Zadanie zrobione")
