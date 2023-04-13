92c21c4a022f9fd78c237879828f422791538615
Mikolaj Mackowiak <73481@student.ue.poznan.pl>
2023-04-13 09:21:32 +0200

    Zadanie 1.4

--- exercise/solo_work/solo_1.py
+++ exercise/solo_work/solo_1.py
@@ -22,3 +22,16 @@ studenci = ["Ania", "Kuba", "Piotr", "Jan"]
 liczba_studentow = len(studenci)
 print("Liczba studentow wynosi: ", liczba_studentow)
 
+# zadanie 1.4
+
+studenci = ["Ania", "Kasia", "Piotr", "Tomek"]
+
+# za pomoca petli i print przywitaj sie z kazdym studentem
+# z tabeli studenci osobno
+# oczekiwany rezultat:
+# Hello Ania
+# Hello Kasia
+# Hello Piotr
+# Hello Tomek
+for x in range(liczba_studentow):
+    print(studenci[x])
