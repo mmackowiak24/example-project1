6c462fd4d7c38dea5590a9f5944afbbbfef67f9c
Mikolaj Mackowiak <73481@student.ue.poznan.pl>
2023-04-13 09:21:09 +0200

    Zadanie 1.3

--- exercise/solo_work/solo_1.py
+++ exercise/solo_work/solo_1.py
@@ -13,5 +13,12 @@ student = input("Wpisz swoje imie: ")
 
 print("Hello "+student)
 
+# zadanie 1.3
 
+studenci = ["Ania", "Kuba", "Piotr", "Jan"]
+
+# policz liczbe studentow w tablicy studenci 
+# oczekiwany rezultat: Liczba studentow wynosi: 4
+liczba_studentow = len(studenci)
+print("Liczba studentow wynosi: ", liczba_studentow)
 
