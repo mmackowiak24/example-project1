bfc8b4ce4ceac8041d37f2873653c399feaededa
Mikolaj Mackowiak <73481@student.ue.poznan.pl>
2023-04-13 09:26:10 +0200

    Zadanie 1.6

--- exercise/solo_work/solo_1.py
+++ exercise/solo_work/solo_1.py
@@ -49,3 +49,15 @@ wynik = pow(liczba,potega)
 # oczekiwany rezultat:
 # Wynik wynosi: 81
 print("Wynik wynosi: ",wynik)
+
+
+# zadanie 1.6
+
+# policz ilosc nawiasow ( w danym ciagu znakow
+
+ciag_znakow = "edbw(hdakqas(skqskahb))adwndwb(wgwidn()dsqwhjdw)"
+liczba_nawiasow_otwierajacych = ciag_znakow.count("(")
+
+# oczekiwany rezultat:
+# Liczba nawiasow otwierajacych wynosi: 4
+print("Liczba nawiasow otwierajacych wynosi: " + str(liczba_nawiasow_otwierajacych))
