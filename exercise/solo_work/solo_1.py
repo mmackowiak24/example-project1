8556d590eba76e08956e8f9f510cdb4db16a71bc
Mikolaj Mackowiak <73481@student.ue.poznan.pl>
2023-04-13 10:55:01 +0200

    Zadanie 1.10

--- exercise/solo_work/solo_1.py
+++ exercise/solo_work/solo_1.py
@@ -103,3 +103,33 @@ for student in studenci:
     if student.split(" ")[-1].lower().startswith("n"):
         studentN += 1
 print("Liczba studentow na N wynosi: ",studentN)
+
+
+# zadanie 1.10
+
+# zmienne poniezej preprezentuja ulozenie punktow na wykresie,
+# do zadania dolaczono takze rysunek pomocniczy
+wykres_1 = [[2, 4], [4, 4], [6, 4]]
+wykres_2 = [[2, 3], [4, 4], [6, 5]]
+wykres_3 = [[2, 3], [4, 3], [5, 4]]
+def isLinear(coordinates):
+    if len(coordinates) < 3:
+        return True #z dwóch punktów można utworzyć funkcję liniową
+    x1, y1 = coordinates[0]
+    x2, y2 = coordinates[1]
+    slope = (y2 - y1 ) / (x2 - x1) if x2 != x1 else float('inf')
+
+    for i in range(2, len(coordinates)):
+        x, y = coordinates[i]
+        new_slope = (y - y1) / (x - x1) if x != x1 else float('inf')
+        if abs(new_slope - slope ) >= 1e-9:
+            return False
+    return True
+# zbadaj kazdy wykres, czy dla wyznaczonych punktow istnieje funkcja
+# liniowa laczaca punkty
+# jesli sie nie da, to zwroc False
+# jesli sie da, zwroc True
+
+print(isLinear(wykres_1))
+print(isLinear(wykres_2))
+print(isLinear(wykres_3))
