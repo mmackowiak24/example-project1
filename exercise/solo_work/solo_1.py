f7d7c762cf31b9fe5ba0da04fec7b86773f16168
Mikolaj Mackowiak <73481@student.ue.poznan.pl>
2023-04-13 09:20:51 +0200

    Zadanie 1.2

--- exercise/solo_work/solo_1.py
+++ exercise/solo_work/solo_1.py
@@ -6,3 +6,12 @@ student = "Ola"
 # oczekiwany rezultat: Hello Ola
 # wykorzystaj w princie zmienne hello i student
 print("{} {}".format(hello,student))
+
+# zadanie 1.2
+
+student = input("Wpisz swoje imie: ")
+
+print("Hello "+student)
+
+
+
