fb009484425593ecb0361ecf6148f67ff7c2f8ff
Mikołaj Maćkowiak <73481@student.ue.poznan.pl>
2023-06-15 23:02:03 +0200

    added prostokat

--- exercise/solo_work/ex_2.py
+++ exercise/solo_work/ex_2.py
@@ -9,3 +9,10 @@ obwod = a + b + c
 pole = int((h * a) / 2)
 
 print("Obwod trojkata wynosi " + str(obwod) + ", zas pole wynisi " + str(pole) + ".")
+
+
+# prostokat
+
+obwod = a + b + a + b
+pole = a * b
+print ("Obwod prostokata wynosi "+ str(obwod) + ", zas pole wynosi " + str(pole) + ".")
